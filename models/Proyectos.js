0f0200248470d2396f9b427f8e9aa689979d2cbb
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-22 14:44:55 -0500

    Se genero URL unico a cada proeycto con slug y shortid

--- models/Proyectos.js
+++ models/Proyectos.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../config/db')
+const slug = require('slug')
+const shortid = require('shortid');
 
 const Proyectos = db.define('proyectos', {
   id: {
@@ -9,6 +11,14 @@ const Proyectos = db.define('proyectos', {
   },
   nombre: Sequelize.STRING,
   url: Sequelize.STRING,
+}, {
+  hooks: {
+    beforeCreate(proyecto) {
+      const url = slug(proyecto.nombre).toLowerCase()
+
+      proyecto.url = `${url}-${shortid.generate()}`
+    }
+  }
 })
 
 module.exports = Proyectos
