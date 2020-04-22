feb87961c21e1918a3f2555f5030d8ca1f415d3b
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-22 14:55:31 -0500

    Se trae los proyectos de la base de datos y los muestra en el layout

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,8 +1,10 @@
 const Proyectos = require('../models/Proyectos');
 
-exports.proyectosHome = (req, res) => {
+exports.proyectosHome = async (req, res) => {
+  const proyectos = await Proyectos.findAll()
   res.render('index', {
-    nombrePagina: 'Proyectos'
+    nombrePagina: 'Proyectos',
+    proyectos
   })
 }
 
