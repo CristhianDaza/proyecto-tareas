f7af7e36e34681de347622aa3f88ba11f021054b
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-22 12:51:57 -0500

    Se agrego validación al formulario de nueva tarea

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const proyectosController = require('../controllers/proyectosController')
+const { body } = require('express-validator/check');
 
 module.exports = function() {
   router.get('/', proyectosController.proyectosHome)
   router.get('/nuevo-proyecto', proyectosController.formularioProyecto)
-  router.post('/nuevo-proyecto', proyectosController.nuevoProyecto)
+  router.post(
+    '/nuevo-proyecto',
+    body('nombre')
+      .not().isEmpty()
+      .trim()
+      .escape(),
+    proyectosController.nuevoProyecto
+  )
   return router
 }
