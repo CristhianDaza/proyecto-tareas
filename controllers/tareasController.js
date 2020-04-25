73a05059400a9da880cd8537f3efd32743292be5
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-24 21:20:11 -0500

    Se envia la tarea a la base de datos

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,3 +1,17 @@
-exports.agregarTarea = (req, res) => {
+const Proyectos = require('../models/Proyectos')
+const Tareas = require('../models/Tareas')
 
+exports.agregarTarea = async (req, res, next) => {
+  const proyecto = await Proyectos.findOne({ where: { url: req.params.url } })
+  const { tarea } = req.body
+  const estado = 0
+  const proyectoId = proyecto.id
+
+  const resultado = await Tareas.create({ tarea, estado, proyectoId } )
+
+  if(!resultado) {
+    return next()
+  }
+
+  res.redirect(`/proyectos/${req.params.url}`)
 }
