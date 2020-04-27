6bf800aaeeae1cca95a82953d46a6496eef10851
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-26 20:45:57 -0500

    Crendo la barra de avance

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -427,7 +427,7 @@ eval("var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;function
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modulos_proyectos__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modulos/proyectos */ \"./public/js/modulos/proyectos.js\");\n/* harmony import */ var _modulos_tareas__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modulos/tareas */ \"./public/js/modulos/tareas.js\");\n\n\n\n//# sourceURL=webpack:///./public/js/app.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modulos_proyectos__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modulos/proyectos */ \"./public/js/modulos/proyectos.js\");\n/* harmony import */ var _modulos_tareas__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modulos/tareas */ \"./public/js/modulos/tareas.js\");\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module './modulos/avance'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\n\n\n//# sourceURL=webpack:///./public/js/app.js?");
 
 /***/ }),
 
